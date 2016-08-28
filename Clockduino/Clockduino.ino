59366f50a37949a58fef8459a8b1a10df94352d3
Fernando Manfredi <contact@acidhub.click>
2016-08-28 02:49:03 -0300

    :fire: Now more more more complete :smile:

--- Clockduino/Clockduino.ino
+++ Clockduino/Clockduino.ino
@@ -25,35 +25,44 @@
 
 #define DS_ADDRESS      0x68 // DS3231 I2C address
 
-#define SEC_REG         0x00 // DS3231 registers position
-#define MIN_REG         0x01
-#define HOUR_REG        0x02
-#define WDAY_REG        0x03
-#define MDAY_REG        0x04
-#define MONTH_REG       0x05
-#define YEAR_REG        0x06
+                  // ADDRESS |  DATA  (ALL data address from Datasheet)
+#define SEC_REG         0x00 // 0xf0 = 10 seconds + 0x0f = seconds
+#define MIN_REG         0x01 // 0xf0 = 10 minutes + 0x0f = minutes
+#define HOUR_REG        0x02 // 0x40 = 12/24 + 0x20 = AM/PM||20 hour + 0x10 = 10hour + 0x0f=hour
+#define WDAY_REG        0x03 // Week day from B00000001 to B00000111 (1 - 7)
+#define MDAY_REG        0x04 // 0x30 = 10 day + 0x0f = day
+#define MONTH_REG       0x05 // 0x80 = Century + 0x10 = 10 month + 0x0f = month
+#define YEAR_REG        0x06 // 0xf0 = 10 year + 0x0f = year
 
-#define AL1SEC_REG      0x07
-#define AL1MIN_REG      0x08
-#define AL1HOUR_REG     0x09
-#define AL1WDAY_REG     0x0A
+#define AL1SEC_REG      0x07 // 0x80 = AL1M1 + 0x70 = 10 seconds + 0x0f = seconds
+#define AL1MIN_REG      0x08 // 0x80 = AL1M2 + 0x70 = 10 minutes + 0x0f = minutes
+#define AL1HOUR_REG     0x09 // 0x80 = AL1M3 + 0x40 = 12/24 + 0x20 = AM/PM||20 hour + 0x10 = 10hour + 0x0f=hour
+#define AL1WDAY_REG     0x0A // 0x80 = AL1M4 + 0x40 = wday/day + 0x30 = 10 day + 0x0f = wday/day
 
-#define AL2MIN_REG      0x0B
-#define AL2HOUR_REG     0x0C
-#define AL2WDAY_REG     0x0D
+#define AL2MIN_REG      0x0B // 0x80 = AL2M2 + 0x70 = 10 minutes + 0x0f = minutes
+#define AL2HOUR_REG     0x0C // 0x80 = AL2M3 + 0x40 = 12/24 + 0x20 = AM/PM||20 hour + 0x10 = 10hour + 0x0f=hour
+#define AL2WDAY_REG     0x0D // 0x80 = AL2M4 + 0x40 = wday/day + 0x30 = 10 day + 0x0f = wday/day
 
-#define CONTROL_REG     0x0E
-#define STATUS_REG      0x0F
-#define AGING_OFFSET_REG 0x0F
-#define TMP_UP_REG      0x11
-#define TMP_LOW_REG     0x12
+#define CONTROL_REG     0x0E // EOSC BBSQW CONV RS2  RS1     INTCN A2IE A1IE
+#define STATUS_REG      0x0F // OSF  0     0    0    EN32kHz BSY   A2F  A1F
+#define AGING_OFFSET_REG 0x10 //SIGN DATA  DATA DATA DATA    DATA  DATA DATA
+#define TMP_UP_REG      0x11 // SIGN DATA  DATA DATA DATA    DATA  DATA DATA (temperature integer)
+#define TMP_LOW_REG     0x12 // DATA DATA  0    0    0       0     0    0    (temperature float)
 
 #define mxClock 12           // 
 #define mxCS 11              // MAX7219 pinout
 #define mxDin 10             //
 
-byte nothing = B00110011;
-byte none = B11001100;
+byte algarism[10][3] = {B00011111, B00010001, B00011111, // 0
+                        B00000000, B00011111, B00000000, // 1
+                        B00011101, B00010101, B00010111, // 2
+                        B00010001, B00010101, B00011011, // 3
+                        B00000111, B00000100, B00011111, // 4
+                        B00010111, B00010101, B00011101, // 5
+                        B00011111, B00010100, B00011100, // 6
+                        B00000001, B00000001, B00011111, // 7
+                        B00011111, B00010101, B00011111, // 8
+                        B00000111, B00000101, B00011111};// 9
 
 LedControl mx=LedControl(mxDin,mxClock,mxCS,2);
 
@@ -61,16 +70,29 @@ void setup() {
     Wire.begin();
     Serial.begin(9600);
     mxConfig();
-    //set(17, 05, 30);
+
+    // Uncomment bellow lines to set your clock
+    //DSsetDate(20, 10, 7, 27, 8, 16);
+    // Syntax: DSsetDate(hour, minute, weekDay, day, month, year);
 }
 
-byte dtob(byte val) { return ((val / 10 * 16) + (val % 10)); }
-byte btod(byte val) { return ((val / 16 * 10) + (val % 16)); }
+void mxConfig(void) {
+  int devices=mx.getDeviceCount();
+  for(int matrix=0;matrix<devices;matrix++) {
+    mx.shutdown(matrix,false);  // Wake up
+    mx.setIntensity(matrix,2);  // set luminosity and
+    mx.clearDisplay(matrix);    // clear.
+  }
+}
+
+// DEC to BIN (DS3231 style) and BIN (DS3231 style) to DEC functions
+byte dtob(byte data) { return ((data / 10 * 16) + (data % 10)); }
+byte btod(byte data) { return ((data / 16 * 10) + (data % 16)); }
 
 void DSset(byte reg, byte data) {
     Wire.beginTransmission(DS_ADDRESS);
-    Wire.write(reg);                        // Move to registrer
-    Wire.write(dtob(data));                 // Write data
+    Wire.write(reg);            // Move to registrer
+    Wire.write(dtob(data));     // Write data
     Wire.endTransmission();
 }
 
@@ -83,25 +105,15 @@ byte DSread(byte reg) {
     return Wire.read();                     // Return with response
 }
 
-void mxConfig(void) {
-  int devices=mx.getDeviceCount();
-  for(int matrix=0;matrix<devices;matrix++) {
-    mx.shutdown(matrix,false);
-    mx.setIntensity(matrix,2);
-    mx.clearDisplay(matrix);
-  }
+void DSsetDate(byte hour, byte minute, byte wday,
+               byte day, byte month, byte year) {
+    DSset(HOUR_REG, dtob(hour));
+    DSset(MIN_REG, dtob(minute));
+    DSset(WDAY_REG, dtob(wday));
+    DSset(MDAY_REG, dtob(day));
+    DSset(MONTH_REG, dtob(month));
+    DSset(YEAR_REG, dtob(year));
 }
-
-byte algarism[10][3] = {B00011111, B00010001, B00011111, // 0
-                        B00000000, B00011111, B00000000, // 1
-                        B00011101, B00010101, B00010111, // 2
-                        B00010001, B00010101, B00011011, // 3
-                        B00000111, B00000100, B00011111, // 4
-                        B00010111, B00010101, B00011101, // 5
-                        B00011111, B00010100, B00011100, // 6
-                        B00000001, B00000001, B00011111, // 7
-                        B00011111, B00010101, B00011111, // 8
-                        B00000111, B00000101, B00011111};// 9
 
 void writeNumber(byte pos, byte number) {
     switch(pos) {
@@ -167,10 +179,10 @@ void displayTime(void) {
     
     switch (wday) {
     case 1:
-      Serial.print("Sunday");
+      Serial.print("Sunday\t");
       break;
     case 2:
-      Serial.print("Monday");
+      Serial.print("Monday\t");
       break;
     case 3:
       Serial.print("Tuesday");
@@ -182,7 +194,7 @@ void displayTime(void) {
       Serial.print("Thursday");
       break;
     case 6:
-      Serial.print("Friday");
+      Serial.print("Friday\t");
       break;
     case 7:
       Serial.print("Saturday");
@@ -192,23 +204,21 @@ void displayTime(void) {
 }
 
 void displayTemp(void) {
-    float temp;
-    byte tUbyte = DSread(TMP_UP_REG);    //Two's complement form
-    byte tLbyte = DSread(TMP_LOW_REG);   //Fractional part
+    byte temp;
+    byte tbyte = DSread(TMP_UP_REG);
   
-    if(tUbyte & 0b10000000) {                   //check if -ve number
-       tUbyte ^= 0b11111111;  
-       tUbyte += 0x1;
-       temp = tUbyte + ((tLbyte >> 6) * 0.25);
-       temp = temp * -1;
+    if(tbyte & B10000000) {             // check if -ve number
+       tbyte ^= B11111111;  
+       tbyte += 0x1;
+       temp = (tbyte *- 1);
     } else {
-        temp = tUbyte + ((tLbyte >> 6) * 0.25); 
+        temp = tbyte;
     }
 
     Serial.print("Temperature is: ");
-    Serial.print(temp, BIN);
-    Serial.print(char(176));                    // EASCII char 176 (°)
-    Serial.print("C");
+    Serial.print(btod(temp));
+    Serial.print(char(176));            // EASCII char 176 (°)
+    Serial.print("C\t");
 
     Serial.println("|\n+=-=-=-=-=-=-=-=-=-=-=-=+");
 }
@@ -216,24 +226,12 @@ void displayTemp(void) {
 void loop() {
     displayTime();
     displayTemp();
+
+    byte working = random(0x00, 0xff);
+    mx.setColumn(0,0, working);
+    mx.setColumn(0,1, ~working);
+    mx.setColumn(1,0, ~working);
+    mx.setColumn(1,1, working);
     
-    switch(nothing) {
-        case B00110011:
-            mx.setColumn(0,0, nothing);
-            mx.setColumn(0,1, none);
-            mx.setColumn(1,0, nothing);
-            mx.setColumn(1,1, none);
-            nothing = B11001100;
-            none = B00110011;
-            break;
-        case B11001100:
-            mx.setColumn(0,0, nothing);
-            mx.setColumn(0,1, none);
-            mx.setColumn(1,0, nothing);
-            mx.setColumn(1,1, none);
-            nothing = B00110011;
-            none = B11001100;
-            break;
-    }
     delay(1000);
 }
